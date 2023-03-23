27c6d8429eeda7b1f786be05f785c0d8d4db2658
alsdn <alsdn@DESKTOP-AN3K4KH>
2023-03-23 15:12:11 +0900

    java hotfix 정보 추가

--- SampleWeb/src/main/java/com/controller/Test.java
+++ SampleWeb/src/main/java/com/controller/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("master 정보 추가");
+		System.out.println("hotfix 정보 추가");
 	}
 
 }
