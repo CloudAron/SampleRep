4f164d7256ed12762ba572ded1e3b231730f1f4a
alsdn <alsdn@DESKTOP-AN3K4KH>
2023-03-23 15:44:24 +0900

    Test third row info add by master

--- SampleWeb/src/main/java/com/controller/Test.java
+++ SampleWeb/src/main/java/com/controller/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("master 정보 추가");
 		System.out.println("master hotfix 삭제 후 master 추가");
+		System.out.println("master 정보 추가");
 	}
 
 }
